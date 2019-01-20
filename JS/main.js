0fbe918490ceea249b7109fe5125ec0ed1c0b1d3
Nadi Adem <NAdem@spartaglobal.com>
2019-01-20 20:09:47 +0000

    Fourth push - slight start on canvas

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,24 @@
-console.log("Hello game world");
+
+console.log("Hello game world"); 
+//var c = canvas.getContext('2d');
+
+let canvas = document.querySelector('canvas');
+function getCanvas(){
+    canvas.width = window.innerWidth; 
+    canvas.height = window.innerHeight;
+}
+function createObj(x,y,w,h){
+    let c = canvas.getContext('2d');
+    c.fillRect(x,y,w,h);
+}
+
+function uniKeyCode(event) {
+    let key = event.uniKeyCode;
+    document.getElementsByTagName('input').innerHTML = "The Unicode KEY code is: " + key;
+    console.log(key);
+}
+
+getCanvas();
+ createObj(100,100,100,100);
+ createObj(300,200,100,100);
+ uniKeyCode();
