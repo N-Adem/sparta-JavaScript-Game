429e3e04df8d7e022d84036e98ad384e6efc1741
Nadi Adem <NAdem@spartaglobal.com>
2019-01-20 20:22:37 +0000

    sixth commit - JS cosde has been cleaned and ready to continue with

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,4 @@
-
-console.log("Hello game world"); 
-//var c = canvas.getContext('2d');
-
+//========================| Game Environment Setup |=====================|
 let canvas = document.querySelector('canvas');
 function getCanvas(){
     canvas.width = window.innerWidth; 
@@ -11,14 +8,10 @@ function createObj(x,y,w,h){
     let c = canvas.getContext('2d');
     c.fillRect(x,y,w,h);
 }
+//===========================| Player Object Setup |=====================|
 
-function uniKeyCode(event) {
-    let key = event.uniKeyCode;
-    document.getElementsByTagName('input').innerHTML = "The Unicode KEY code is: " + key;
-    console.log(key);
-}
-
+//===========================| Game Initialization |=====================|
 getCanvas();
- createObj(100,100,100,100);
- createObj(300,200,100,100);
- uniKeyCode();
+createObj(100,100,100,100);
+createObj(300,200,100,100);
+ 
