73ed0c65d993fd0edb3146a68881cdbd4aefb33c
Nadi Adem <NAdem@spartaglobal.com>
2019-01-24 08:52:05 +0000

    seventh update - changes include spawn object system, and a change to the player script

--- JS/main.js
+++ JS/main.js
@@ -1,54 +1,114 @@
-//========================| Game Environment Setup |=====================|
-//----| Public variables for canvas funcrtions & game flow|----
-let gameStart = true;
-let sectionClear = false;
-let gameEnd = false;
-let canvas = document.getElementById('mCanvas');
-let c = canvas.getContext('2d');
-//let p = canvas.getContext('2d');
-//===========| Creates the canvas background|==============|
-function getCanvas() {
+document.addEventListener("DOMContentLoaded", function () {
+    //========================| Game Environment Setup |=====================|
+    //----| Public variables for canvas funcrtions & game flow|----
+    let canvas = document.querySelector('canvas');
+    let context = canvas.getContext('2d');
+    //==================| Creates the canvas background|==============|
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-}
-// =========|Creates the puddles and draws them on screen within an interval time frame|=============|
-function puddleObj(w, h) {
-    for (let i = 0; i < 4; i++) {
-        let x = Math.random() * window.innerWidth;
-        let y = Math.random() * window.innerHeight;
-        c.fillStyle = "blue";
-        c.fillRect(x, y, w, h);
+    //=====================| Puddle Spawn |=======================
+    function GenPuddle(xpos, ypos, width, height) {
+        this.xpos = xpos;
+        this.ypos = ypos;
+        context.fillStyle = "blue";
+        context.fillRect(xpos, ypos, width, height);
+        function draw() {
+            context.clearRect(0, 0, canvas.innerWidth, canvas.innerHeight);
+        }
     }
-}
-function draw() {
-    c.clearRect(0, 0, canvas.width, canvas.height);
-    puddleObj(100, 100);
-}
-//===============| Manages the game states |====================|
-/*function gameFlow() {
-    if (gameStart == true) {
-        setInterval(draw, 2000);
-    } else if (sectionClear == true) {
-        c.clearRect(puddleObj);
-        gameStart = true;
+    //======================| Player Object|========================
+    function Obj(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        context.fillStyle = "green";
+        context.fillRect(x, y, w, h);
+        function animate() {
+            requestAnimationFrame(animate);
+            context.clearRect(0, 0, canvas.innerWidth, canvas.innerHeight);
+            let player1 = new Obj(x,y,50,50);
+            document.addEventListener('keydown', function () {
+                if (event.key === "w") {
+                    console.log("player y -pos" + player1.y);
+                    player1.y -= 1;
+                }
+                if (event.key === "s") {
+                    console.log("player y +pos" + player1.y);
+                    player1.y += 1;
+                }
+                if (event.key === "a") {
+                    console.log("player x -pos" + player1.x);
+                    player1.x -= 1;
+                }
+                if (event.key === "d") {
+                    player1.x += 1;
+                    console.log("player x +pos" + player1.x);
+                }
+            })
+        }
     }
-}*/
-//===========================| Player Object Setup |=====================|
-class Player {
-    constructor(context){
-        this.context = context;
+    //=========================| Spawn Puddle|===============================
+    function SpawnPuddle() {
+        let swamp = [];
+        for (let i = 0; i < 4; i++) {
+            let x = Math.random() * window.innerWidth;
+            let y = Math.random() * window.innerHeight;
+            let puddle3 = new GenPuddle(x, y, 100, 100);
+            swamp.push(puddle3);
+        }
     }
-    playerInit(){
-        this.context.clearRect(0, 0, canvas.width, canvas.height);
-        this.context.fillRect(600,300,100,100);
-    }
-}
+    //==========================================================================
+    
+    
+    //===========================================================================================
 
-//=======================| class declaration |==============|
-//let player1 = new Player(c);
-//===========================| Game Initialization |=====================|
-//getCanvas();
-//setInterval(draw, 2000);
-//console.log(player1.playerInit);
+    // =======================| Player create |=============================
+    /*
+        let xPos = 100;
+        let mXpos = 1;
+        let yPos = 100;
+        let radius = 30;
+        function animate() {
+            requestAnimationFrame(animate);
+            // c.clearRect(0, 0, innerWidth, innerHeight);
+            c.beginPath();
+            c.arc(xPos, yPos, radius, 0, 2 * Math.PI);
+            c.strokeStyle = 'yellow';
+            c.fillStyle = "red";
+            c.stroke();
+            document.addEventListener('keydown', function () {
+                if (event.key === "w") {
+                    console.log("Up key has been pressed");
+                    yPos -= mXpos;
+                }
+                if (event.key === "s") {
+                    console.log("Down key has been pressed");
+                    yPos += mXpos;
+                }
+                if (event.key === "a") {
+                    console.log("Left key has been pressed");
+                    xPos -= mXpos;
+                }
+                if (event.key === "d") {
+                    console.log("Right key has been pressed");
+                    xPos += mXpos;
+                }
+            })
+    
+        }*/
+    //==================| Check Distance |===================|
+    function checkDistance(x1, y1, x2, y2) {
+        let xDist = x1 - x2;
+        let yDist = y1 - y2;
+        return Math.sqrt(Math.pow(xDist, 2)
+            + Math.pow(yDist, 2));
+    }
+    //============================| Calling function|=============
+    let player1 = new Obj(100, 100, 50, 50);
+    SpawnPuddle();
+  
+    //console.log(checkDistance(player1.xpos, player1.ypos, puddle3.xpos, puddle3.ypos));
 
-
+})//======= | END OF DOM EVENT LISTENER
+// f- init ()
+// f- update (draw())
+// f - animate
--- JS/main2.js
+++ JS/main2.js
@@ -1,61 +1,73 @@
-console.log('registered');
-let canvas = document.getElementById('myCanvas');
-let context = canvas.getContext('2d');
-//===================|  Class is for the canvas display  |===================|
-class CanvasScreen {
-    constructor(canvas, context) {
-        this.canvas = canvas;
-        this.context = context;
-    }
-    getCanvas() {
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
-    }
-}
-//===================|class is for the puddle spawn system|================|
-let puddleArray = []; 
-class Puddle {
-    constructor(context, canvas,array) {
-        this.context = context;
-        this.canvas = canvas;
-        this.array = array;
-    }
-    createPuddle() { 
-        for(let i = 0; i < 4; i++){
-            let x =  Math.random() * window.innerWidth;
-            let y =  Math.random() * window.innerHeight;
-            this.context.fillStyle ="blue";
-            this.context.fillRect(x, y, 150, 75);
+document.addEventListener("DOMContentLoaded", function () {
+    console.log('Project intialized');
+    let canvas = document.getElementById('myCanvas');
+    let context = canvas.getContext('2d');
+    //===================|  Class is for the canvas display  |===================|
+    class CanvasScreen {
+        constructor(canvas, context) {
+            this.canvas = canvas;
+            this.context = context;
+        }
+        getCanvas() {
+            this.canvas.width = window.innerWidth;
+            this.canvas.height = window.innerHeight;
         }
     }
-    drawPuddle() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.array.push(setInterval(this.createPuddle(),3000));
+    //===================|class is for the puddle spawn system|================|
+    let puddleArray = [];
+    let xPos = 100;
+    let yPos = 400;
+    class Object {
+        constructor(context, canvas, array, color) {
+            this.context = context;
+            this.canvas = canvas;
+            this.array = array;
+            this.color = color;
+        }
+        createPuddle() {
+            for (let i = 0; i < 4; i++) {
+                let x = Math.random() * window.innerWidth;
+                let y = Math.random() * window.innerHeight;
+                this.context.fillStyle = this.color;
+                this.context.fillRect(x, y, 150, 150);
+            }
+        }
+        createPlayer() {
+            this.context.beginPath();
+            this.context.arc(xPos, yPos, 50, 0, 2 * Math.PI);
+            this.context.strokeStyle = 'yellow';
+            this.context.stroke();
+        }
+        drawPuddle() {
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.array.push(this.createPuddle());
+        }
+        
+        
     }
-}
-//===================|   class if for the player object   |================|
-class Player {
-    constructor(context, canvas) {
-        this.context = context;
-        this.canvas = canvas;
-    }
-    createPlayer() {
-        this.context.translate(canvas.width / 2, canvas.height / 2);
-        this.context.fillStyle ="red";
-        this.context.fillRect(150, 200, 150, 75);
-    }
-    drawPlayer() {
-        this.createPlayer();
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-}
-//===================|             class init             |================|
-let display = new CanvasScreen(canvas, context);
-let puddle1 = new Puddle(context, canvas,puddleArray);
-let player1 = new Player(context, canvas);
-//===================|    Game obj management and init    |================|
-display.getCanvas();
-puddle1.createPuddle();
-puddle1.drawPuddle();
-player1.createPlayer();
-player1.drawPlayer();
+    // ======================================
+    document.addEventListener("keydown", function (e) {
+        let key_press = String.fromCharCode(event.keyCode);
+        if (key_press === "W") {
+            console.log("Up key has been pressed");
+            player1.x +=2;
+        }
+        if (event.key === "s") {
+            console.log("Down key has been pressed");
+        }
+        if (event.key === "a") {
+            console.log("Left key has been pressed");
+        }
+        if (event.key === "d") {
+            console.log("Right key has been pressed");
+        }
+    })
+    //===================|             class init             |================|
+    let display = new CanvasScreen(canvas, context);
+    let puddle1 = new Object(context, canvas, puddleArray, "lightBlue");
+    let player1 = new Object(context, canvas, null, null);
+    //===================|    Game obj management and init    |================|
+    display.getCanvas();
+    puddle1.createPuddle();
+    player1.createPlayer();
+})// ========================================================| End of the Document Event listener |================================================================
