8df0f65f53a428c4e81beff53967df35d3a50703
Nadi Adem <NAdem@spartaglobal.com>
2019-01-24 10:33:13 +0000

    eigth commit - collision detection is complete

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
+    console.log("game Init");
     //========================| Game Environment Setup |=====================|
     //----| Public variables for canvas funcrtions & game flow|----
     let canvas = document.querySelector('canvas');
     let context = canvas.getContext('2d');
+    let img = document.getElementById('imageWater');
     //==================| Creates the canvas background|==============|
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -11,12 +13,14 @@ document.addEventListener("DOMContentLoaded", function () {
         this.xpos = xpos;
         this.ypos = ypos;
         context.fillStyle = "blue";
+        //context.drawImage(img, 10, 10);
         context.fillRect(xpos, ypos, width, height);
         function draw() {
             context.clearRect(0, 0, canvas.innerWidth, canvas.innerHeight);
         }
     }
     //======================| Player Object|========================
+
     function Obj(x, y, w, h) {
         this.x = x;
         this.y = y;
@@ -25,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
         function animate() {
             requestAnimationFrame(animate);
             context.clearRect(0, 0, canvas.innerWidth, canvas.innerHeight);
-            let player1 = new Obj(x,y,50,50);
+            let player1 = new Obj(x, y, 50, 50);
             document.addEventListener('keydown', function () {
                 if (event.key === "w") {
                     console.log("player y -pos" + player1.y);
@@ -47,55 +51,22 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
     //=========================| Spawn Puddle|===============================
-    function SpawnPuddle() {
+    function SpawnPuddle(player) {
         let swamp = [];
         for (let i = 0; i < 4; i++) {
             let x = Math.random() * window.innerWidth;
             let y = Math.random() * window.innerHeight;
             let puddle3 = new GenPuddle(x, y, 100, 100);
+
             swamp.push(puddle3);
+            if (checkDistance(player1.x, player1.y, puddle3.xpos, puddle3.ypos) <= 100) {
+                console.log("there has been a collision");
+                console.log("p x is =", player1.x, "p y is =", player1.y);
+                console.log("puddle position x is =", puddle3.xpos, "puddle pos y is =", puddle3.ypos);
+            }
         }
     }
-    //==========================================================================
-    
-    
-    //===========================================================================================
-
-    // =======================| Player create |=============================
-    /*
-        let xPos = 100;
-        let mXpos = 1;
-        let yPos = 100;
-        let radius = 30;
-        function animate() {
-            requestAnimationFrame(animate);
-            // c.clearRect(0, 0, innerWidth, innerHeight);
-            c.beginPath();
-            c.arc(xPos, yPos, radius, 0, 2 * Math.PI);
-            c.strokeStyle = 'yellow';
-            c.fillStyle = "red";
-            c.stroke();
-            document.addEventListener('keydown', function () {
-                if (event.key === "w") {
-                    console.log("Up key has been pressed");
-                    yPos -= mXpos;
-                }
-                if (event.key === "s") {
-                    console.log("Down key has been pressed");
-                    yPos += mXpos;
-                }
-                if (event.key === "a") {
-                    console.log("Left key has been pressed");
-                    xPos -= mXpos;
-                }
-                if (event.key === "d") {
-                    console.log("Right key has been pressed");
-                    xPos += mXpos;
-                }
-            })
-    
-        }*/
-    //==================| Check Distance |===================|
+    //==================| Check Distance |=======================================|
     function checkDistance(x1, y1, x2, y2) {
         let xDist = x1 - x2;
         let yDist = y1 - y2;
@@ -103,11 +74,8 @@ document.addEventListener("DOMContentLoaded", function () {
             + Math.pow(yDist, 2));
     }
     //============================| Calling function|=============
-    let player1 = new Obj(100, 100, 50, 50);
-    SpawnPuddle();
-  
-    //console.log(checkDistance(player1.xpos, player1.ypos, puddle3.xpos, puddle3.ypos));
-
+    let player1 = new Obj(100, 200, 50, 50);
+    SpawnPuddle(player1);
 })//======= | END OF DOM EVENT LISTENER
 // f- init ()
 // f- update (draw())
