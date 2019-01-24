50551dbd438486d4c6eed551cf7601403c177a49
Nadi Adem <NAdem@spartaglobal.com>
2019-01-24 18:09:18 +0000

    13

--- JS/main.js
+++ JS/main.js
@@ -7,8 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
     let img = document.getElementById('imageWater');
     let pScore = document.getElementById('pscore');
     //==================| Creates the canvas background|==============|
-    canvas.width = window.innerWidth / 1.5;
+    canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
     //=====================| Puddle Spawn |=======================
     function GenPuddle(xpos, ypos, width, height) {
         this.xpos = xpos;
@@ -16,9 +17,6 @@ document.addEventListener("DOMContentLoaded", function () {
         context.fillStyle = "blue";
         //context.drawImage(img, 10, 10);
         context.fillRect(xpos, ypos, width, height);
-        function draw() {
-            context.clearRect(0, 0, innerWidth, innerHeight);
-        }
     }
     //======================| Player Object|========================
     var dx = 1;
@@ -30,13 +28,8 @@ document.addEventListener("DOMContentLoaded", function () {
         context.fillRect(x, y, w, h);
     }
     let player1 = new Obj(100, 200, 50, 50);
-
-    function update() {
-        requestAnimationFrame(update);
-        context.clearRect(0, 0, canvas.innerWidth, canvas.innerHeight);
-    }
     //=========================================================================================================================================================================
-    
+
     var isLeft = false;
     var isRight = false;
     var isUp = false;
@@ -47,84 +40,163 @@ document.addEventListener("DOMContentLoaded", function () {
             console.log("player y -pos" + player1.y);
             isUp = true;
             isDown = false;
-            isLeft =false;
+            isLeft = false;
             isRight = false;
         }
         if (event.key === "s") {
             console.log("player y +pos" + player1.y);
             isUp = false;
             isDown = true;
-            isLeft =false;
+            isLeft = false;
             isRight = false;
         }
         if (event.key === "a") {
             console.log("player x -pos" + player1.x);
             isUp = false;
             isDown = false;
-            isLeft =true;
+            isLeft = true;
             isRight = false;
         }
         if (event.key === "d") {
             console.log("player x +pos" + player1.x);
             isUp = false;
             isDown = false;
-            isLeft =false;
+            isLeft = false;
             isRight = true;
         };
     });
-    // update();
-    var x = 20;
-    var y = 20;
-    function play() {
-        requestAnimationFrame(play);
-        context.clearRect(0, 0, context.innerWidth, context.innerHeight);
 
+    var xp = 70;
+    var yp = 70;
+    //====================| variables for the pawns |================================================================================================
+    let x1 = Math.random() * window.innerWidth;
+    let y1 = Math.random() * window.innerHeight;
+    let x2 = Math.random() * window.innerWidth;
+    let y2 = Math.random() * window.innerHeight;
+    let x3 = Math.random() * window.innerWidth;
+    let y3 = Math.random() * window.innerHeight;
+    let x4 = Math.random() * window.innerWidth;
+    let y4 = Math.random() * window.innerHeight;
+    function Bucket(posx, posy) {
+        this.posx = posx;
+        this.posy = posy;
         context.beginPath();
-        context.arc(x, y, 30, 0, Math.PI * 2, false);
-        context.strokeStyle = "blue";
+        context.arc(xp, yp, 30, 0, Math.PI * 2, false);
+        context.strokeStyle = "yellow";
+        context.lineWidth = 40;
         context.stroke();
-
-        if (isUp === true) {
-            y-=1;
-        }
-        if (isDown === true) {
-            y+=1;
-        }
-        if (isLeft === true) {
-            x-=1;
-        }
-        if (isRight === true) {
-            x+=1;
-        }
-
     }
+    var score = 1;
+    var newScore = 0;
+    var isAction1 = true;
+    var isAction2 = true;
+    var isAction3 = true;
+    var isAction4 = true;
+    var isNext = true;
+    function init() {
+        //let player1 = new Obj(100, 200, 50, 50);
 
-play();
-    //=========================| Spawn Puddle|===============================
-    let score = 1;
-    let newScore = 0;
-    console.log("score is = ", newScore);
-    function SpawnPuddle(player) {
-        let swamp = [];
-        for (let i = 0; i < 4; i++) {
-            let x = Math.random() * window.innerWidth;
-            let y = Math.random() * window.innerHeight;
-            let puddle3 = new GenPuddle(x, y, 100, 100);
-
-            swamp.push(puddle3);
-            if (checkDistance(player1.x, player1.y, puddle3.xpos, puddle3.ypos) <= 200) {
+        let buck = new Bucket(xp, yp);
+        if (isAction1 === true) {
+            let puddle1 = new GenPuddle(x1, y1, 100, 100);
+            if (checkDistance(buck.posx, buck.posy, puddle1.xpos, puddle1.ypos) <= 70) {
                 console.log("---there has been a collision");
-                console.log("p x is =", player1.x, "p y is =", player1.y);
-                console.log("puddle position x is =", puddle3.xpos, "puddle pos y is =", puddle3.ypos);
                 //----------| Point system |----------
                 newScore += score;
                 console.log("new score is =", newScore);
-                // document.getElementById('pscore').innerHTML = parseInt(newScore);
-                context.fillStyle = "red";
-                context.font = "80px Arial";
-                context.fillText(newScore, 10, 80);
+                isAction1 = false;
+                //document.getElementById('pscore').innerHTML = parseInt(newScore);
             }
         }
+        if (isAction2 === true) {
+            let puddle2 = new GenPuddle(x2, y2, 100, 100);
+            if (checkDistance(buck.posx, buck.posy, puddle2.xpos, puddle2.ypos) <= 70) {
+                console.log("---there has been a collision");
+                //----------| Point system |----------
+                newScore += score;
+                console.log("new score is =", newScore);
+                isAction2 = false;
+                //document.getElementById('pscore').innerHTML = parseInt(newScore);
+            }
+        }
+        //-----------------------------------------------------
+        if (isAction3 === true) {
+            let puddle3 = new GenPuddle(x3, y3, 100, 100);
+            //console.log("score is = ", newScore);
+            if (checkDistance(buck.posx, buck.posy, puddle3.xpos, puddle3.ypos) <= 70) {
+                console.log("---there has been a collision");
+                //----------| Point system |----------
+                newScore += score;
+                console.log("new score is =", newScore);
+                isAction3 = false;
+                //document.getElementById('pscore').innerHTML = parseInt(newScore);
+            }
+        }
+        if (isAction4 === true) {
+            let puddle4 = new GenPuddle(x4, y4, 100, 100);
+            if (checkDistance(buck.posx, buck.posy, puddle4.xpos, puddle4.ypos) <= 70) {
+                console.log("---there has been a collision");
+                //----------| Point system |----------
+                newScore += score;
+                console.log("new score is =", newScore);
+                isAction4 = false;
+                //document.getElementById('pscore').innerHTML = parseInt(newScore);
+            }
+        }
+        //---------------------------------------------------------------------------------
+        context.fillStyle = "red";
+        context.font = "80px Arial";
+        context.fillText(newScore, 10, 80);
+
+        //isAction3 = true;
+    }
+    function animate() {
+        requestAnimationFrame(animate);
+        context.clearRect(0, 0, innerWidth, innerHeight);
+        //--------------------------------------------------------------------------------
+        if (isNext === true) {
+            init();
+        }
+        //------------------------------------------------------------------------------
+
+        if (isUp === true) {
+            yp -= 15;
+        }
+        if (isDown === true) {
+            yp += 15;
+        }
+        if (isLeft === true) {
+            xp -= 15;
+        }
+        if (isRight === true) {
+            xp += 15;
+        }
+    }
+
+    animate();
+    //=========================| Spawn Puddle|=========================================================================================================================================
+
+    //console.log("score is = ", newScore);
+    function SpawnPuddle(buck) {
+        let swamp = [];
+
+        let x = Math.random() * window.innerWidth;
+        let y = Math.random() * window.innerHeight;
+        let puddle3 = new GenPuddle(x, y, 100, 100);
+        swamp.push(player1);
+        if (checkDistance(player1.x, player1.y, puddle3.xpos, puddle3.ypos) <= 200) {
+            console.log("---there has been a collision");
+            //console.log("p x is =", buck.x, "p y is =", player1.y);
+            console.log("puddle position x is =", puddle3.xpos, "puddle pos y is =", puddle3.ypos);
+            //----------| Point system |----------
+            newScore += score;
+            console.log("new score is =", newScore);
+            //document.getElementById('pscore').innerHTML = parseInt(newScore);
+            context.fillStyle = "red";
+            context.font = "80px Arial";
+            context.fillText(newScore, 10, 80);
+        }
+
     }
     //==================| Check Distance |=======================================|
     function checkDistance(x1, y1, x2, y2) {
@@ -134,7 +206,7 @@ play();
             + Math.pow(yDist, 2));
     }
     //============================| Calling function|=============
-    SpawnPuddle(player1);
+    // SpawnPuddle(player1);
 })//======= | END OF DOM EVENT LISTENER
 //----------| Code notes for the character movement |-------------
 // f- init ()
